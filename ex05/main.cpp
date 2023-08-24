0b7a841b8719c270cd8458064ec692f76478fc95
Nico Reher <nreher@student.42berlin.de>
2023-08-24 17:11:51 +0200

    HARLLLLLLLLLLLLLLLLLLLLLL

---
+++ ex05/main.cpp
@@ -0,0 +1,15 @@
+#include "Harl.hpp"
+
+int main (void)
+{
+	Harl karen;
+
+	karen.complain("info");
+	karen.complain("debug");
+	karen.complain("warning");
+	karen.complain("error");
+	karen.complain("a");
+	karen.complain("b");
+	karen.complain("c");
+
+}
