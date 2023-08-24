1992a5e65561efde580acfe1603183c1be7b71ea
Nico Reher <nreher@student.42berlin.de>
2023-08-24 13:29:13 +0200

    ex04 added

---
+++ ex04/cpyfrplstr.hpp
@@ -0,0 +1,9 @@
+#ifndef CPYFRPLSTR_H
+#define CPYFRPLSTR_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+#endif
---
+++ ex04/main.cpp
@@ -0,0 +1,28 @@
+#include "cpyfrplstr.hpp"
+
+int main (int argc, char *argv[])
+{
+	std::ifstream	input;
+	std::ofstream	output;
+	std::string		new_name;
+	size_t			repl = 0;
+
+	if ( argc != 4)
+	{
+		std::cout << "incorrect amout of arguments!" << std::endl;
+		return (1);
+	}
+	new_name += argv[1];
+	new_name += ".replace";
+	output.open(new_name.c_str(), std::ios::trunc);
+	input.open(argv[1], std::ios::in);
+	std::string content((std::istreambuf_iterator<char>(input)), (std::istreambuf_iterator<char>()));
+	repl = content.find(argv[2]);
+	while (repl != std::string::npos)
+	{
+		content.erase(repl, strlen(argv[2]));
+		content.insert(repl, argv[3]);
+		repl = content.find(argv[2], repl + strlen(argv[3]));
+	}
+	output << content;
+}
